0f350baea8c8cb8528e9189f8c3bb99b19533c7b
Mustafa Dokumaci <mustafa@foraa.com>
2016-09-13 19:03:45 +0200

    mocking now api

--- test/state.js
+++ test/state.js
@@ -8,7 +8,39 @@ const now = { api: () => {} }
 
 test('list - generate', t => {
   const nowApi = sinon.stub(now, 'api')
-  const state = proxyquire('../lib/state', {'../lib/now': nowApi})
+  nowApi['@global'] = true
+  const state = proxyquire('../lib/state', { '../now': nowApi })
+
+  var cbs = {}
+  var emitter = {
+    on: (e, cb) => {
+      cbs[e] = cb
+      return emitter
+    },
+    once: (e, cb) => {
+      cbs[e] = cb
+      return emitter
+    },
+    send: () => {
+      cbs['data']({ uid: 1, name: 's1', url: 'u1.sh', created: 11 })
+      cbs['data']({ uid: 2, name: 's1', url: 'u2.sh', created: 12 })
+      cbs['data']({ uid: 3, name: 's1', url: 'u3.sh', created: 13 })
+      cbs['data']({ uid: 4, name: 's1', url: 'u4.sh', created: 21 })
+      cbs['data']({ uid: 5, name: 's1', url: 'u5.sh', created: 22 })
+      cbs['data']({ uid: 6, name: 's2', url: 'u6.sh', created: 11 })
+      cbs['data']({ uid: 7, name: 's2', url: 'u7.sh', created: 21 })
+      cbs['data']({ uid: 8, name: 's2', url: 'u8.sh', created: 22 })
+      cbs['data']({ uid: 9, name: 's3', url: 'u9.sh', created: 11 })
+      cbs['data']({ uid: 10, name: 's4', url: 'u10.sh', created: 11 })
+      cbs['data']({ uid: 99, name: 's10', url: 'u99.sh', created: 11 })
+      cbs['end']()
+      return emitter
+    }
+  }
+
+  nowApi
+    .withArgs('deployments', 'API-TOKEN', 'deployments.*')
+    .returns(emitter)
 
   state.start('API-TOKEN')
 
